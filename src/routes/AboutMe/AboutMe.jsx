c4fe37125f5e90729d2b984904666464aee44c04
Yeferson Rojas Olcunche <yefersonolcunche@gmail.com>
2021-11-15 15:14:28 -0500

    update

--- src/routes/AboutMe/AboutMe.jsx
+++ src/routes/AboutMe/AboutMe.jsx
@@ -1,5 +1,3 @@
-import TagCloud from '../../components/LearnedTools/LearnedTools'
-import Apre from './Aprend.json'
 import './AboutMe.css'
 
 const AboutMe = () => {
@@ -15,12 +13,22 @@ const AboutMe = () => {
             <p >Soy un apasionado por el desarrollo web, me interesa la ciberseguridad por el cual realicé un curso en Suite Hacking una academia Mexicana en el 2020 y en ese mismo año ingrese a la Universidad a estudiar Ingeniería de sistemas, me gustan los deportes extremos, también trabajé como guía turístico en un ecoparque en donde aprendí a trabajar en equipo y la atención al cliente.
             </p>
             <div className="icons">
-                
-            {
-                Apre.map(items=>
-                    <TagCloud key={items.id} items={items}/>
-                    )
-                }
+                <h2>Skills</h2>
+                <div className="kills">
+                    <ul>
+                        <li><p>PYTHON</p></li>
+                        <li><p>CSS3</p></li>
+                        <li><p>JAVASCRIPT</p></li>
+                        <li><p>REACT</p></li>
+                        <li><p>HTML5</p></li>
+                        <li><p>ANGULAR</p></li>
+                        <li><p>MYSQL</p></li>
+                        <li><p>FIGMA</p></li>
+                        <li><p>DJANGO</p></li>
+                        <li><p>MONGODB</p></li>
+                    
+                    </ul>
+                </div>
 
                 </div>
                 </div>
